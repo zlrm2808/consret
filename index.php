4e6d24d6d2a5b44782e87c60d46a0a07cd511ea3
zlrm2808 <zlrm2808@gmail.com>
2022-08-10 15:56:49 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -10,40 +10,55 @@
 
 <body>
     <?php
-    $serverName = "PROGRAMADOR-02";
-    $connectionInfo = array("Database" => "F2099");
-    $conn = sqlsrv_connect($serverName, $connectionInfo);
+        date_default_timezone_set('America/Caracas');
+        $serverName = "PROGRAMADOR-02";
+        $connectionInfo = array("Database" => "F2099");
+        $conn = sqlsrv_connect($serverName, $connectionInfo);
 
-    if ($conn) {
-        echo "Conexión establecida.<br />";
-    } else {
-        echo "Conexión no se pudo establecer.<br />";
-        die(print_r(sqlsrv_errors(), true));
-    }
+        if ($conn) {
+            echo "Conexión establecida.<br />";
+        } else {
+            echo "Conexión no se pudo establecer.<br />";
+            die(print_r(sqlsrv_errors(), true));
+        }
 
-    $sql = (
-            "SELECT open_p,
-                    IMP_nc_open_nompro 
-            FROM IMPP2001 
-            WHERE IMP_nc_open_ncompr = 'CPP22-0000729'"
-            );
-    $stmt = sqlsrv_query($conn, $sql);
-    if ($stmt === false) {
-        die(print_r(sqlsrv_errors(), true));
-    }
+        $sql = ("SELECT CONVERT(VARCHAR, IMP_nc_open_feccon, 103) AS Fecha_Emision,
+                        IMP_nc_open_numfac,
+                        IMP_nc_open_nreten
+                FROM IMPP2001
+                INNER JOIN DYNAMICS.dbo.SY01500 on INTERID = DB_NAME() 
+                "
+                );
+        $stmt = sqlsrv_query($conn, $sql);
+        if ($stmt === false) {
+            die(print_r(sqlsrv_errors(), true));
+        }
 
-    // Make the first (and in this case, only) row of the result set available for reading.
-    if (sqlsrv_fetch($stmt) === false) {
-        die(print_r(sqlsrv_errors(), true));
-    }
+        if ($stmt === false) {
+            die(print_r(sqlsrv_errors(), true));
+        }
 
-    // Get the row fields. Field indices start at 0 and must be retrieved in order.
-    // Retrieving row fields by name is not supported by sqlsrv_get_field.
-    $rif = sqlsrv_get_field($stmt, 0);
-    $prov = sqlsrv_get_field($stmt, 1);
-    echo "$rif ";
-    echo "<br>";
-    echo "$prov ";
+        $table = '
+            <table border=1>
+                <tr>
+                    <th><h2>Fecha de Emisión</h2></th>
+                    <th><h2>Numero de Factura</h2></th>
+                    <th><h2>Numero de Comprobante</h2></th>
+                </tr>
+            ';
+
+        while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
+        $table .= '
+            <tr>
+                <td>' . $row["Fecha_Emision"] . '</td>
+                <td>' . $row["IMP_nc_open_numfac"] . '</td>
+                <td>' . $row["IMP_nc_open_nreten"] . '</td>
+            </tr>';
+        }
+        $table .= '</table>';
+        echo $table;
+        sqlsrv_free_stmt($stmt);
+        sqlsrv_close($conn);    
     ?>
 </body>
 
