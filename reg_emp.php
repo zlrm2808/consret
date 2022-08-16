b1dc3f401d88d5baee768130ccfa4fb8d21f5377
Zeus Rodriguez <[zlrm2808@gmail.com]>
2022-08-16 16:02:04 -0400

    commit

--- reg_emp.php
+++ reg_emp.php
@@ -1,31 +1,43 @@
+<head>
+    <script>
+        function esperar(espera) {
+            string = "pausa_alerta(" + espera + ");";
+            setTimeout(string, espera);
+        }
+
+        function pausa_alerta(espera) {
+            alert("Ok " + espera / 1000 + " Segundos");
+        }
+    </script>
+</HEAD>
+
 <?php
 
-    include_once("conexion.php");
+include_once("conexion.php");
 
-    $usuario = $_POST["Usuario"];
-    $password = $_POST["Contraseña"];
-    $password2 = $_POST["Contraseña2"];
+$usuario = $_POST["Usuario"];
+$password = $_POST["Contraseña"];
+$password2 = $_POST["Contraseña2"];
 
 $sql = ("SELECT PV_MI_idprov,
                 PV_MI_nompro
         FROM IMPP0161
-        WHERE PV_MI_idprov = '". $usuario ."' ;
+        WHERE PV_MI_idprov = '" . $usuario . "' ;
         "
 );
 
 $stmt = sqlsrv_query($conn, $sql);
 if ($stmt === false) {
     echo "EL RIF INDICADO NO SE ENCUENTRA EN NUESTRA BASE DE DATOS";
-}
-else {
+} else {
     echo '<script>alert("Registro Exitoso!!")</script>';
-    die;
-    header("Location: login.html");
-    exit();
+    
+    //header("Location: login.html");
+    //exit();
+    
     if ($password != $password2) {
         echo "Las contraseñas no coinciden";
-    }
-    else {
+    } else {
         $sql = ("INSERT INTO ");
     }
 }
